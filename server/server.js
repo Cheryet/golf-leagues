5a84c9a4189352e03bcac014377bdf1555e0fa66
Cheryet <C.heryet@outlook.com>
2022-12-19 12:37:12 -0700

    add get route for all players json data

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const db = require("./database");
 
+// ~ MIDDLEWARE ~
 const app = express();
 const port = process.env.PORT || 8001;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(morgan("combined"));
+
+// ~ ROUTES ~
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+//Get All Players (JSON)
+app.get("/players", (req, res) => {
+  db.getAllPlayers().then((data) => {
+    res.send(data);
+  });
 });
 
 app.listen(port, () => {
