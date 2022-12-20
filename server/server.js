a2dc05f41dcaa15cd64b277437617e1bc87de5ad
Cheryet <C.heryet@outlook.com>
2022-12-19 19:25:41 -0700

    update route for single player

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const db = require("./database");
 
-// ~ MIDDLEWARE ~
+// ~~ MIDDLEWARE ~~
 const app = express();
 const port = process.env.PORT || 8001;
 
@@ -11,16 +11,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
-// ~ ROUTES ~
+// ~~ ROUTES ~~
 
+// ~ HOME ROUTE ~
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-//Get All Players (JSON)
-app.get("/players", (req, res) => {
+// ~ PLAYERS ROUTES ~
+
+//Get All Players
+app.get("/api/players", (req, res) => {
   db.getAllPlayers().then((data) => {
-    res.send(data);
+    const players = data;
+    res.send(players);
+  });
+});
+
+//Get players by Id
+app.get("/api/players/:id", (req, res) => {
+  db.getPlayerById(req.params.id).then((data) => {
+    const player = data;
+    res.send(player);
   });
 });
 
