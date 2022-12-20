4b0eb322a0e75600744abb9d7bb20712d8ff3b7e
Cheryet <C.heryet@outlook.com>
2022-12-20 15:36:58 -0700

    add seeds for leagues table

--- server/db/seeds/01_seeds.sql
+++ server/db/seeds/01_seeds.sql
@@ -5,11 +5,17 @@ VALUES
 (1, 'John', 'Doe', 'JDoe@example.com', 2.3),
 (2, 'Billy', 'McNight', 'Nighty@example.com', 14);
 
-INSERT INTO teams (name, captain_id, sponsor_id)
+INSERT INTO teams (name, captain_id, sponsor_id, league_id)
 VALUES 
-('Darters', 1, 1),
-('Hookers', 2, 3);
+('Darters', 1, 1, 1),
+('Hookers', 2, 3, 1);
 
 INSERT INTO scores (player_id, score, week, front_nine, back_nine)
 VALUES
 (1, 40, 1, true, false);
+
+INSERT INTO leagues (name)
+VALUES
+(`Mens League`)
+("SR. Mens League")
+('Womens League')
