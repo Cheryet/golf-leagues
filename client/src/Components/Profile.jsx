c120d53458f32164dd6f649bd5fc8509618feba7
Cheryet <C.heryet@outlook.com>
2022-12-21 14:52:43 -0700

    populate component

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import "./Profile.scss";
+
 const Profile = () => {
-  return <></>;
+  return (
+    <section className="profile-container">
+      <div className="profile-img"></div>
+      <p className="profile-username">Lethbridge Country Club</p>
+      <p className="profile-leagues">Leagues: 2</p>
+      <p className="profile-players">Players: 87</p>
+    </section>
+  );
 };
 
 export default Profile;
