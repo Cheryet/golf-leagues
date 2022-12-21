217fa1b7d58dd117cea6d1cf0a08fc78d031dc2a
Cheryet <C.heryet@outlook.com>
2022-12-20 21:00:30 -0700

    update to use navbar component

---
+++ client/src/Components/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Navbar from "./Navbar";
+import { Route, Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
