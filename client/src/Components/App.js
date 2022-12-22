e004213486bc8256e1ef3f39754a39c99fc36d25
Cheryet <C.heryet@outlook.com>
2022-12-22 15:53:13 -0700

    add routing for pages

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,26 @@
 import "./App.scss";
 import Navbar from "./Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Notfound from "../pages/Notfound";
+import Leagues from "../pages/leagues/Leagues";
+import CreateLeague from "../pages/leagues/CreateLeague";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/dashboard"} element={<Dashboard />} />
+          <Route path={"/leagues"}>
+            <Route index element={<Leagues />} />
+            <Route path={"create"} element={<CreateLeague />} />
+          </Route>
+          <Route path="*" element={<Notfound />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
