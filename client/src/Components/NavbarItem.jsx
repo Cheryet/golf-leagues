404dd1eefd53dd087214abbbf470b3e7c24abcd0
Cheryet <C.heryet@outlook.com>
2022-12-21 18:38:21 -0700

    create navbarItem component to tidy up

---
+++ client/src/Components/NavbarItem.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import * as RiIcons from "react-icons/ri";
+import SubnavItem from "./SubnavItem";
+
+const NavbarItem = (props) => {
+  //Subnav state
+  const [subnav, setSubnav] = useState(false);
+
+  const showSubnav = () => {
+    setSubnav(!subnav);
+  };
+
+  //Remove null from props
+
+  return (
+    <>
+      {props.subnav ? (
+        <>
+          <li
+            className={subnav ? props.className + " active" : props.className}
+          >
+            <Link to={props.path} onClick={showSubnav}>
+              {props.icon}
+              <span>{props.title}</span>
+              <RiIcons.RiArrowDropDownLine className="subnav-arrow" />
+            </Link>
+          </li>
+          <div className={subnav ? "subnav-list active" : "subnav-list"}>
+            <SubnavItem subnav={props.subnav} />
+          </div>
+        </>
+      ) : (
+        <li className={props.className}>
+          <Link to={props.path}>
+            {props.icon}
+            <span>{props.title}</span>
+          </Link>
+        </li>
+      )}
+    </>
+  );
+};
+
+export default NavbarItem;
