3dfe742741d53a3784901c0dace9793fd935dfaf
Cheryet <C.heryet@outlook.com>
2022-12-23 16:16:03 -0700

    assign state with ajax get requests

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -6,8 +6,36 @@ import Home from "../pages/Home";
 import Notfound from "../pages/Notfound";
 import Leagues from "../pages/leagues/Leagues";
 import CreateLeague from "../pages/leagues/CreateLeague";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { RiContactsBookUploadLine } from "react-icons/ri";
+import Teams from "../pages/teams/Teams";
+import CreateTeam from "../pages/teams/CreateTeam";
 
 function App() {
+  //Initial State Declaration
+  const [state, setState] = useState({
+    teams: {},
+    players: {},
+    scores: {},
+  });
+
+  useEffect(() => {
+    //Ajax Get request for data
+    const teamsPromise = axios.get("/api/teams");
+    const playersPromise = axios.get("/api/players");
+    const scoresPromise = axios.get("/api/scores");
+
+    Promise.all([teamsPromise, playersPromise, scoresPromise]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        teams: all[0].data,
+        players: all[1].data,
+        scores: all[2].data,
+      }));
+    });
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -18,6 +46,13 @@ function App() {
           <Route path={"/leagues"}>
             <Route index element={<Leagues />} />
             <Route path={"create"} element={<CreateLeague />} />
+          </Route>
+          <Route path={"/teams"}>
+            <Route
+              index
+              element={<Teams teams={state.teams} players={state.players} />}
+            />
+            <Route path={"create"} element={<CreateTeam />} />
           </Route>
           <Route path="*" element={<Notfound />} />
         </Routes>
