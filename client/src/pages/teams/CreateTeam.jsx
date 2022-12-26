671e8b27affe4ab4db02d6bf3b4eddb4e1d53106
Cheryet <C.heryet@outlook.com>
2022-12-26 13:52:09 -0700

    add form for new team input

--- client/src/pages/teams/CreateTeam.jsx
+++ client/src/pages/teams/CreateTeam.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./CreateTeam.scss";
+import { useState } from "react";
 
 const CreateTeam = () => {
-  return <></>;
+  const [teamName, setTeamName] = useState("");
+
+  return (
+    <>
+      <div className="new-team">
+        <h1>Create Team</h1>
+        <div className="new-team-container">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              className="team-name-input"
+              type="text"
+              placeholder="Enter New Team's Name"
+              onChange={(e) => setTeamName(e.target.value)}
+            />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CreateTeam;
