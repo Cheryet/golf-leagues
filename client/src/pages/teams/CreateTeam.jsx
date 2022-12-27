18e8727f3708c813ce937ff2d9fe5970497f8f01
Cheryet <C.heryet@outlook.com>
2022-12-27 15:44:19 -0700

    add new team form

--- client/src/pages/teams/CreateTeam.jsx
+++ client/src/pages/teams/CreateTeam.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./CreateTeam.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CreateTeam = () => {
   const [teamName, setTeamName] = useState("");
+
+  //Set Page Title
+  useEffect(() => {
+    document.title = "Golf Leagues | Create Team";
+  });
 
   return (
     <>
@@ -15,10 +20,28 @@ const CreateTeam = () => {
               e.preventDefault();
             }}
           >
+            <label>Team Name: </label>
             <input
               className="team-name-input"
+              name="team-name-input"
               type="text"
               placeholder="Enter New Team's Name"
+              onChange={(e) => setTeamName(e.target.value)}
+            />
+            <label>Sponsors Name: </label>
+            <input
+              className="sponsor-name-input"
+              name="sponsor-name-input"
+              type="text"
+              placeholder="Enter Sponsors Name"
+              onChange={(e) => setTeamName(e.target.value)}
+            />
+            <label>Captains Name: </label>
+            <input
+              className="captaian-name-input"
+              name="captaian-name-input"
+              type="text"
+              placeholder="Enter Captains Name"
               onChange={(e) => setTeamName(e.target.value)}
             />
           </form>
