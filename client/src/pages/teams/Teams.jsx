514d6d70592a27e33ba357e838a9df7649504c35
Cheryet <C.heryet@outlook.com>
2023-01-26 10:54:31 -0700

    add print all btn

--- client/src/pages/teams/Teams.jsx
+++ client/src/pages/teams/Teams.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { BsLayoutTextWindow } from "react-icons/bs";
 import TeamItem from "./TeamItem";
+import * as AiIcons from "react-icons/ai";
 import "./Teams.scss";
 
 const Teams = (props) => {
+  //Helper - Gets all Teams
   const teams = props.teams.map((item, index) => {
     return (
       <TeamItem
@@ -16,7 +19,17 @@ const Teams = (props) => {
     );
   });
 
-  return <>{teams}</>;
+  return (
+    <>
+      <section className="teams-menu">
+        <div className="btn-container">
+          <AiIcons.AiOutlinePrinter className="print-all-icon" />
+          <button className="print-all-btn">Print all Teams</button>
+        </div>
+      </section>
+      {teams}
+    </>
+  );
 };
 
 export default Teams;
